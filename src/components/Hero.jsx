1b3e456b59fa22efb3ceca83e8af739a47590e64
bhanu2006-24 <bhanupsaini2024@gmail.com>
2025-12-14 19:17:39 +0530

    implemented some video part

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
 import { useGSAP } from "@gsap/react"
 import { SplitText } from "gsap/all"
 import gsap from "gsap"
+import { useRef } from "react"
+import { useMediaQuery } from "react-responsive"
 
 const Hero = () => {
+
+
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     useGSAP(() => {
         const heroSplit = new SplitText(".title", { type: "chars , words" });
 
@@ -36,13 +44,11 @@ const Hero = () => {
                 scrub: 1
             }
         })
-            .to(".right-leaf", {
-                y: 200
-            }, 0)
-            .to(".left-leaf", {
-                y: -200
-            }, 0)
+            .to(".right-leaf", { y: 200 }, 0)
+            .to(".left-leaf", { y: -200 }, 0)
 
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
 
     }, []);
 
@@ -77,6 +83,11 @@ const Hero = () => {
                 </div>
 
             </section>
+
+            <div className="absolute video inset-0">
+                <video ref={videoRef} src="/videos/input.mp4" muted playsInline preload="auto" ></video>
+            </div>
+
         </>
     )
 }
