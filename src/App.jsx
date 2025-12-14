0d9c713ceb49a8443714d3c3744938eb6c9f9875
bhanu2006-24 <bhanupsaini2024@gmail.com>
2025-12-14 18:15:46 +0530

    added left & right leaf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from './components/Navbar'
+import Hero from './components/Hero';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
 
     <main>
       <Navbar />
+      <Hero />
 
     </main>
   )
